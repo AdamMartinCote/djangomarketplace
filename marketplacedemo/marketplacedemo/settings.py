cf0a0ba19497a26b18d72881e356595f3358eb02
Adam Martin-Côté <adammartincote@gmail.com>
2019-01-19 11:59:55 -0500

    db changes

--- marketplacedemo/marketplacedemo/settings.py
+++ marketplacedemo/marketplacedemo/settings.py
@@ -109,3 +109,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_URL = '/static/'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
