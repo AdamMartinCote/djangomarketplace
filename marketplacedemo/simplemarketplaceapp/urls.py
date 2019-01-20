cc80ff1557d158f416b97e5bfb50c778512cb336
Adam Martin-Côté <adammartincote@gmail.com>
2019-01-20 15:55:24 -0500

    removed dead code

--- marketplacedemo/simplemarketplaceapp/urls.py
+++ marketplacedemo/simplemarketplaceapp/urls.py
@@ -5,7 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='index'),
-    # path('<int:pk>/', views.detail, name='detail'),
     path('cart/', views.cart, name='cart'),
     path('purchase/', views.purchase, name='purchase'),
 ]
